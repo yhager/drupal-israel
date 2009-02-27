4773a9ab253e4294232e465e27df5ae39e0e2a05
yuval <yuval@48625804-3a9d-4787-a64b-8d71f29a12a4>
2009-02-27 11:44:15 +0000

    added user block
    git-svn-id: svn+ssh://svn.linnovate.net/home/svn/doil@83 48625804-3a9d-4787-a64b-8d71f29a12a4

---
+++ sites/all/modules/custom/custom-user-block.tpl.php
@@ -0,0 +1,6 @@
+<?php // $Id$ ?>
+<?php print $vars['hello']; ?>
+<span class="links"><?php print $vars['links']; ?></span>
+<hr />
+<p><?php print $vars['status']; ?><br /><?php print $vars['users_list'] ?></p>
+<p><?php print $vars['latest']; ?></p>
