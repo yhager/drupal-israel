7d43b1532401d85d2f47920ce659eee6a7c3df8c
zohar <zohar@48625804-3a9d-4787-a64b-8d71f29a12a4>
2009-02-27 12:22:04 +0000

    changes to Yuval's code
    git-svn-id: svn+ssh://svn.linnovate.net/home/svn/doil@84 48625804-3a9d-4787-a64b-8d71f29a12a4

--- sites/all/modules/custom/custom-user-block.tpl.php
+++ sites/all/modules/custom/custom-user-block.tpl.php
@@ -1,5 +1,5 @@
 <?php // $Id$ ?>
-<?php print $vars['hello']; ?>
+<h2><?php print $vars['hello']; ?></h2>
 <span class="links"><?php print $vars['links']; ?></span>
 <hr />
 <p><?php print $vars['status']; ?><br /><?php print $vars['users_list'] ?></p>
