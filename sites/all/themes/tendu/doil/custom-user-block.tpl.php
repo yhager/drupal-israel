3fa6c7626c3ec19d5237e16b3c27a4f4d5d8923f
tom <tom@48625804-3a9d-4787-a64b-8d71f29a12a4>
2009-03-02 00:51:10 +0000

    styling user block header
    git-svn-id: svn+ssh://svn.linnovate.net/home/svn/doil@97 48625804-3a9d-4787-a64b-8d71f29a12a4

---
+++ sites/all/themes/tendu/doil/custom-user-block.tpl.php
@@ -0,0 +1,7 @@
+<?php // $Id: custom-user-block.tpl.php 84 2009-02-27 12:22:04Z zohar $ ?>
+<div class="block-header">
+  <h2><?php print $vars['hello']; ?></h2>
+  <span class="links"><?php print $vars['links']; ?></span>
+</div>
+<p><?php print $vars['status']; ?><br /><?php print $vars['users_list'] ?></p>
+<p><?php print $vars['latest']; ?></p>
